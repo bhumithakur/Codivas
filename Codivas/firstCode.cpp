483bd992129c360d7991fab0fcda2ccd0641168a
BHOOMIKA SINGH <75352017+bhumithakur@users.noreply.github.com>
2021-12-04 08:31:52 -0800

    Add files via upload
    first session for members of codivas

---
+++ Codivas/firstCode.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Header Files ->  The files that tell the compiler how to call some functionality (without knowing how the functionality actually works)
+using namespace std; // computer needs to know the code for the cout, cin functionalities and  it needs to know which namespace they are defined.                      
+
+ int main(){ // from where the main program starts
+ cout<<"Hello Codivas!!"<<endl; // It will print -> Hello Codivas!!
+  return 0; // we return 0 , because we use int type main function , if we use void type the we don't need to return 
+ }                    
---
+++ Codivas/operatorInC++.cpp
@@ -0,0 +1,26 @@
+// BY CODIVAS :)
+#include <iostream>
+using namespace std;
+int main(){
+    // int num1 = 5, num2 = 7, sum; // instead of this we can ask user to iput the values
+    int num1, num2, sum;
+     cin>>num1>>num2; //( take input from user)
+    sum = num1 + num2 ;// Addition
+    cout<<sum<<endl; // it will print -> 12
+    int subtraction;
+    subtraction = num1 - num2; // subtraction
+    cout<<subtraction<<endl; // it will print -> -2
+        int  multiplication;
+    multiplication = num1*num2; //  multiplication
+    cout<<multiplication<<endl; // it will print ->  35
+     
+        int  division;
+     division = num2/num1; //  division
+    cout<<division<<endl; // it will print -> 1
+       int   modulus;
+    modulus = num2 % num1; // modulus operator -> it gives us the remainder
+    cout<< modulus<<endl; // it will print -> 2
+   
+    
+
+}
